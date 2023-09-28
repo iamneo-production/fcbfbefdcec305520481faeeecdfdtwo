c58216ae8e9f364a5a2f45087017b567eac20bab
Iamneo <techadmin@iamneo.ai>
2023-09-28 06:53:51 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -1,28 +1,14 @@
-// package com.examly.springapp;
-
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
-// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-// public class SpringappApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(SpringappApplication.class, args);
-// 	}
-
-// }
-
-
 package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-@SpringBootApplication
-class SpringappApplication {
 
-    public static void main(String[] args) {
-            SpringApplication.run(SpringAppApplication.class, args);
-                }
-                }
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringappApplication.class, args);
+	}
+
+}
