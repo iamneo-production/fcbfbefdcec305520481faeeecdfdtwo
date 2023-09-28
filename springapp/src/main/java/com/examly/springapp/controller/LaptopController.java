864e3936fed1f1c1e919a8018048976c86896ad8
Iamneo <techadmin@iamneo.ai>
2023-09-28 07:35:16 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
+++ springapp/src/main/java/com/examly/springapp/controller/LaptopController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/laptops")
+@RequestMapping("/api/laptop")
 public class LaptopController {
     @Autowired
     private ApiService apiService;
